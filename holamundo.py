72fd5f77bd9d0408bd82f576a6dfd76146845989
MicaelaSalvvador <sacmi9@hotmail.com>
2024-01-26 15:53:59 -0600

    primerosPasosconPython

---
+++ holamundo.py
@@ -0,0 +1,4 @@
+#EN PYTHON NO ES NECESARIO EL TIPO DE VARIABLE
+mensaje ='Hola Mundo'
+mensaje2="Estoy  aprendiendo un nuevo lenguaje de programacion"
+print(mensaje + "\n" + mensaje2 )
---
+++ print.py
@@ -0,0 +1,43 @@
+print('Hola Mundo, y sean bienvenidos al mundo de python')
+print()
+print('Mi nombre es Micaela  y  quiero ensenarles  a programar en Python')
+print()
+print('Hola como estas \n y como te llamas')
+print("Hola chicos quiero contarles que me ire a  pasear",end="       "+ "Y la pasare bien")
+print()
+print()
+print("Lewandosky","Asu Fati","Ferran Torres","Pedri","Gavi","Ter Stegen",sep="-")
+
+print()
+print()
+print()
+
+print("     **        **")
+print("   *     *    *     *")
+print(" *         * *         *")
+print("*           **           **")
+print("*                         *")
+print(" *                       *")
+print("   *                   *")
+print("     *               *")
+print("       *           *")
+print("         *       *")
+print("           *   *")
+print("             **")
+
+print()
+print()
+cadena ="Hola,que tal"
+
+print(cadena[0])
+print(cadena[6])
+print(cadena[3])
+print()
+print()
+subcadena=cadena[0:4]
+print(subcadena)
+
+print()
+a=2
+b=8
+print("El valor de la variable a vale",a,"El valor de la variable b vale",b)
