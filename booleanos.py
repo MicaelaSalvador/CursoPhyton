7e5eb5e7ad20decc1a120ce3bcd03f4687ba027e
MicaelaSalvvador <sacmi9@hotmail.com>
2024-01-27 13:23:41 -0600

    operadores

---
+++ booleanos.py
@@ -0,0 +1,11 @@
+a=5
+b=5
+print(a == b)
+# print()
+
+# c=5
+# d=8
+# print(c != d)
+
+# print(a>b)
+# print(a<b)
---
+++ operadoresLogicos.py
@@ -0,0 +1,11 @@
+a=True
+b=False
+print(a and b)
+
+a=2
+b=3
+c=4
+d=10
+print(a==b or c<d)
+print(a==b and c<d)
+
